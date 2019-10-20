ca78949950f7bb8940fed0e97c8571dbffa2dd77
journey <admin@libaoka.com>
2019-10-20 12:09:26 +0800

    update axios constructor methods

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const model = require('./lib');
 module.exports = model;
+
 /**
 let btcExplorer = new model.btcExplorer('btc');
-
-btcExplorer.getUnTx().then(res=>{
-  console.log(res);
-});
+    btcExplorer.getUnTx().then(res=>{
+      console.log(res);
+    });
 
 let blockIoExplorer = new model.blockIoExplorer('Your Api KEY');
+    blockIoExplorer.getBalance().then(res=>{
+      console.log(res);
+    });
 
-blockIoExplorer.getBalance().then(res=>{
-  console.log(res);
-});
-
-let etherscanIoExplorer = new model.etherscanIoExplorer('mainnetcn');
-etherscanIoExplorer.getSupply().then(res=>{
-  'use strict'
-  console.log(res);
-});
-**/
+let etherscanIoExplorer = new model.etherscanIoExplorer('mainnet');
+    etherscanIoExplorer.getSupply().then((res)=>{
+      console.log(res);
+    });
+ **/
